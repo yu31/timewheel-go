5d536fab0211905aec0ef3c239f77d781917b55d
wuyu <wuyu@yunify.com>
2022-02-21 02:10:49 +0800

    Add test case for context.ContextFunc in Timer.Close

--- timer_test.go
+++ timer_test.go
@@ -3,6 +3,7 @@ package timewheel
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -39,10 +40,207 @@ func TestTimer_Close(t *testing.T) {
 	require.Equal(t, b.timers.Len(), 0)
 }
 
-func TestTimer_Close_With_AfterFunc(t *testing.T) {
-	// TODO:
+func TestTimer_Close_With_TimeFunc1(t *testing.T) {
+	tw := Default()
+	defer tw.Stop()
+	tw.Start()
+
+	ctxCancel, cancelFunc := context.WithCancel(context.Background())
+	waitRun := make(chan struct{})
+
+	timer := tw.TimeFunc(
+		ctxCancel, time.Now(),
+		func(ctx context.Context) error {
+			close(waitRun)
+			ticker := time.NewTimer(time.Second * 1)
+		LOOP:
+			for {
+				select {
+				case <-ticker.C:
+				case <-ctx.Done():
+					cancelFunc()
+					break LOOP
+				}
+			}
+			ticker.Stop()
+			return nil
+		},
+	)
+
+	var isRun bool
+	select {
+	case <-waitRun:
+		isRun = true
+	case <-time.After(time.Second):
+	}
+	require.True(t, isRun)
+
+	timer.Close()
+
+	var isDone bool
+	select {
+	case <-ctxCancel.Done():
+		isDone = true
+	case <-time.After(time.Second):
+	}
+
+	require.True(t, isDone)
 }
 
-func TestTimer_Close_With_Schedule(t *testing.T) {
-	// TODO:
+func TestTimer_Close_With_TimeFunc2(t *testing.T) {
+	tw := Default()
+	defer tw.Stop()
+	tw.Start()
+
+	ctxCancel, cancelFunc := context.WithCancel(context.Background())
+	waitRun := make(chan struct{})
+
+	var jobCtx context.Context
+
+	timer := tw.TimeFunc(
+		ctxCancel, time.Now(),
+		func(ctx context.Context) error {
+			ticker := time.NewTimer(time.Second * 1)
+		LOOP:
+			for {
+				select {
+				case <-ticker.C:
+				case <-ctx.Done():
+					jobCtx = ctx
+					close(waitRun)
+					break LOOP
+				}
+			}
+			ticker.Stop()
+			return nil
+		},
+	)
+
+	cancelFunc()
+
+	var isRun bool
+	select {
+	case <-waitRun:
+		isRun = true
+		require.NotNil(t, jobCtx)
+	case <-time.After(time.Second):
+	}
+	require.True(t, isRun)
+
+	var isDone bool
+	select {
+	case <-jobCtx.Done():
+		isDone = true
+	case <-time.After(time.Second):
+	}
+	require.True(t, isDone)
+
+	require.NotPanics(t, func() {
+		timer.Close()
+	})
+}
+
+func TestTimer_Close_With_Schedule1(t *testing.T) {
+	tw := Default()
+	defer tw.Stop()
+	tw.Start()
+
+	waitRun := make(chan struct{})
+	ctxCancel, cancelFunc := context.WithCancel(context.Background())
+
+	timer := tw.ScheduleJob(
+		ctxCancel,
+		ScheduleFunc(func(t time.Time) time.Time {
+			return t.Add(time.Millisecond * 10)
+		}),
+		JobFunc(func(ctx context.Context) error {
+			close(waitRun)
+			ticker := time.NewTimer(time.Second * 1)
+		LOOP:
+			for {
+				select {
+				case <-ticker.C:
+				case <-ctx.Done():
+					cancelFunc()
+					break LOOP
+				}
+			}
+			ticker.Stop()
+			return nil
+		}),
+	)
+
+	var isRun bool
+	select {
+	case <-waitRun:
+		isRun = true
+	case <-time.After(time.Second):
+	}
+	require.True(t, isRun)
+
+	timer.Close()
+
+	var ok bool
+	select {
+	case <-ctxCancel.Done():
+		ok = true
+	case <-time.After(time.Second):
+	}
+
+	require.True(t, ok)
+}
+
+func TestTimer_Close_With_Schedule2(t *testing.T) {
+	tw := Default()
+	defer tw.Stop()
+	tw.Start()
+
+	ctxCancel, cancelFunc := context.WithCancel(context.Background())
+	waitRun := make(chan struct{})
+	var jobCtx context.Context
+
+	timer := tw.ScheduleJob(
+		ctxCancel,
+		ScheduleFunc(func(t time.Time) time.Time {
+			return t.Add(time.Millisecond * 10)
+		}),
+		JobFunc(func(ctx context.Context) error {
+			ticker := time.NewTimer(time.Second * 1)
+		LOOP:
+			for {
+				select {
+				case <-ticker.C:
+				case <-ctx.Done():
+					jobCtx = ctx
+					close(waitRun)
+					break LOOP
+				}
+			}
+			ticker.Stop()
+			return nil
+		}),
+	)
+
+	cancelFunc()
+
+	var isRun bool
+	select {
+	case <-waitRun:
+		isRun = true
+		require.NotNil(t, jobCtx)
+	case <-time.After(time.Second):
+	}
+	require.True(t, isRun)
+
+	var isDone bool
+	select {
+	case <-jobCtx.Done():
+		isDone = true
+	case <-time.After(time.Second):
+	}
+	require.True(t, isDone)
+
+	require.NotPanics(t, func() {
+		timer.Close()
+	})
 }
